6bc8984f8a29616adaef3e41107dca6bc34e04d6
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2024-11-19 12:46:05 +0200

    modify the primary id name for te region and district controllers

--- HRMS.Presentation/Controllers/DistrictController.cs
+++ HRMS.Presentation/Controllers/DistrictController.cs
@@ -2,71 +2,69 @@
 using HRMS.DAL.DTOs;
 using HRMS.DAL.Handler;
 using HRMS.DAL.Interfaces;
-using HRMS.DAL.ModelsDto;
 using HRMS.DAL.TypeRepository;
 using HRMS.DAL.UnitOfWork;
 using HRMS.Presentation.Handlers;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HRMS.Presentation.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [ApiExceptionHandler]
-    [Authorize]
-    public class DistrictController : ControllerBase
-    {
-        private readonly IUnitOfWork _unitOfWork;
-        IGenericRepository<District> districtRepository;
+	[Route("api/[controller]")]
+	[ApiController]
+	[ApiExceptionHandler]
+	[Authorize]
+	public class DistrictController : ControllerBase
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		IGenericRepository<District> districtRepository;
 
-        public DistrictController(IUnitOfWork unitOfWork)
-        {
-            this._unitOfWork = unitOfWork;
-            districtRepository = new DistrictRepository(_unitOfWork, "District", "regionid", "MSORG");
-        }
+		public DistrictController(IUnitOfWork unitOfWork)
+		{
+			this._unitOfWork = unitOfWork;
+			districtRepository = new DistrictRepository(_unitOfWork, "District", "DistrictId", "MSORG");
+		}
 
-        [HttpGet("[action]/{regionid}/{orgid}")]
-        public async Task<ActionResult<IEnumerable<District>>> GetDistrict(int regionid, int orgid)
-        {
-            Dictionary<string, int> whereConditionsDic = new Dictionary<string, int>
-            {
-                { "regionid", regionid },
-                { "orgid", orgid }
-            };
-            var data = await districtRepository.GetListByCustomFields(whereConditionsDic);
-            return data;
-        }
-
-
-        [HttpPost("[action]")]
-        public async Task<ActionResult<District>> CreateDistrict(DistrictDto District)
-        {
-            if (TryValidateModel(District))
-            {
-                MappingHandler mapping = new MappingHandler();
-                District _District = mapping.Map<District>(District);
-                return await districtRepository.Add(_District);
-            }
-            else
-                return BadRequest();
-
-        }
+		[HttpGet("[action]/{regionid}/{orgid}")]
+		public async Task<ActionResult<IEnumerable<District>>> GetDistrict(int regionid, int orgid)
+		{
+			Dictionary<string, int> whereConditionsDic = new Dictionary<string, int>
+			{
+				{ "regionid", regionid },
+				{ "orgid", orgid }
+			};
+			var data = await districtRepository.GetListByCustomFields(whereConditionsDic);
+			return data;
+		}
 
 
-        [HttpPut("[action]/{id}")]
-        public async Task<IActionResult> UpdateDistrict(int id, DistrictDto District)
-        {
-            if (TryValidateModel(District))
-            {
-                MappingHandler mapping = new MappingHandler();
-                District _District = mapping.Map<District>(District);
-                return await districtRepository.Update(id, _District);
-            }
-            else
-                return BadRequest();
+		[HttpPost("[action]")]
+		public async Task<ActionResult<District>> CreateDistrict(DistrictDto District)
+		{
+			if (TryValidateModel(District))
+			{
+				MappingHandler mapping = new MappingHandler();
+				District _District = mapping.Map<District>(District);
+				return await districtRepository.Add(_District);
+			}
+			else
+				return BadRequest();
 
-        }
-    }
+		}
+
+
+		[HttpPut("[action]/{id}")]
+		public async Task<IActionResult> UpdateDistrict(int id, DistrictDto District)
+		{
+			if (TryValidateModel(District))
+			{
+				MappingHandler mapping = new MappingHandler();
+				District _District = mapping.Map<District>(District);
+				return await districtRepository.Update(id, _District);
+			}
+			else
+				return BadRequest();
+
+		}
+	}
 }
